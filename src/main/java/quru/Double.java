f342c4e868df3c36e2352f7f8e1452da2b8494e9
Vatsook1996 <ahmetdianovv@mail.ru>
2023-04-09 14:49:48 +0300

    Addendum

---
+++ src/main/java/quru/Double.java
@@ -0,0 +1,15 @@
+package quru;
+
+public class Double {
+    public static void main(String[] args) {
+
+
+        int numberInt = 80;
+        double numberdouble = 12.142;
+
+        System.out.println("numberInt + numberdouble =" + (numberInt + numberdouble));
+        System.out.println("numberInt * numberdouble =" + (numberInt * numberdouble));
+        System.out.println("numberInt / numberdouble =" + (numberInt / numberdouble));
+    }
+}
+
