76902cca6dc73800743778100cc9a5291f956f96
Vatsook1996 <ahmetdianovv@mail.ru>
2023-04-09 14:17:19 +0300

    Axded2

---
+++ src/main/java/quru/comparisons.java
@@ -0,0 +1,14 @@
+package quru;
+
+public class comparisons {
+
+    public static void main(String[] args) {
+        int c = 10;
+        int f = 15;
+        if (c < f) {
+            System.out.println("Первое число меньше второго");
+        }
+
+    }
+
+}
--- src/main/java/quru/main.java
+++ src/main/java/quru/main.java
@@ -3,21 +3,11 @@ package quru;
 public class main {
 
     public static void main(String[] args) {
-        int a = 30;
-        int b = 45;
-        int c = 65;
-        int d = 67;
-        System.out.println("a + b = " + (a + b));
-        System.out.println("a - b = " + (a - b));
-        System.out.println("a * b = " + (a * b));
-        System.out.println("b / a = " + (b / a));
-        System.out.println("b % a = " + (b % a));
-        System.out.println("c % a = " + (c % a));
-        System.out.println("a++   = " +  (a++));
-        System.out.println("b--   = " +  (a--));
-        // Проверьте разницу в d++ и ++d
-        System.out.println("d++   = " +  (d++));
-        System.out.println("++d   = " +  (++d));
+        int a = 6;
+        int b = 4;
+        if (a > b) {
+            System.out.println("Первое число больше второго");
+        }
 }
 
 }
